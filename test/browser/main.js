c492732217489015c6742c6d550b72c24ed256ed
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-10-05 16:42:38 +0500

    updates

--- test/browser/main.js
+++ test/browser/main.js
@@ -9,10 +9,14 @@ async function initWasm() {
     }
 }
 
-async function runLexer() {
+async function checkWASM() {
     if (!wasmModule) {
         await initWasm();
     }
+}
+
+async function runLexer(noAlert = false) {
+    await checkWASM();
 
     const inputString = prompt("Enter JUSTC code:");
     
@@ -33,7 +37,8 @@ async function runLexer() {
             );
             const result = JSON.stringify(resultjson);
             
-            alert(result);
+            if (!noAlert) alert(result);
+            return result
         } catch (error) {
             console.error("Error:", error);
             alert("JUSTC/core/lexer.cpp error.");
@@ -41,6 +46,44 @@ async function runLexer() {
     } else {
         alert("Cancelled: No input provided.");
     }
+}
+
+async function runParser(input_) {
+    await checkWASM();
+
+    const inputString = input_ || prompt("Enter JUSTC JSON tokens:");
+
+    if (inputString && inputString.length > 0) {
+        try {
+            const inputJSON = JSON.parse(inputString);
+            const resultptr = wasmModule.ccall(
+                'parser',
+                'number',
+                ['string'],
+                [inputJSON]
+            );
+            const resultjson = JSON.parse(wasmModule.UTF8ToString(resultptr));
+            wasmModule.ccall(
+                'free_string',
+                null,
+                ['number'],
+                [resultptr]
+            );
+            const result = JSON.stringify(resultjson);
+
+            alert(result);
+            return result
+        } catch (error) {
+            console.error("Error:", error);
+            alert("JUSTC/core/parser.cpp error.");
+        }
+    } else {
+        alert("Cancelled: No input provided.");
+    }
+}
+
+async function parseJUSTC() {
+    return await runParser(await runLexer(true));
 }
 
 initWasm();

221e461bbb99dc9e29eebf62fef3a13d8eaadd29
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-10-08 18:51:25 +0500

    Update justc.sh

--- test/linux/justc.sh
+++ test/linux/justc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# sudo apt-get update
-# sudo apt-get install -y libcurl4-openssl-dev cmake build-essential pkg-config
+sudo apt-get update
+sudo apt-get install -y libcurl4-openssl-dev cmake build-essential pkg-config
 
 mkdir build
 cd build

8d4f4ac5893408746256bdb883b8127b0b5e8291
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-10-08 18:48:08 +0500

    updates

--- test/linux/justc.sh
+++ test/linux/justc.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
+
+# sudo apt-get update
+# sudo apt-get install -y libcurl4-openssl-dev cmake build-essential pkg-config
+
 mkdir build
 cd build
 cmake ..
 make
 
-FILE=test/linux/test.justc
+FILE=../test/linux/test.justc
 
 ./justc $FILE
 
 RESULT=$(cat $FILE)
-ECHO $RESULT
+echo $RESULT

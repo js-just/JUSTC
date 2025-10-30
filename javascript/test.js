a195e305dee6a01ab4fd6239002133edc8b1d28e
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-10-31 00:45:13 +0500

    Update test.js

--- javascript/test.js
+++ javascript/test.js
@@ -1,5 +1,5 @@
 require('./justc.node.js');
-require('./justc.js').then((JUSTC)=>{
+(async ()=>{return await require('./justc.js')})().then((JUSTC)=>{
     console.log(typeof JUSTC, JUSTC, typeof JUSTC.initialize);
     JUSTC.initialize().then(()=>{
         console.log(JSON.stringify(JUSTC.execute('a is 123456.')))

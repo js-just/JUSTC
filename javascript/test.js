b026a82357aae1ef0aba47916b856547083172b3
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-10-31 01:18:36 +0500

    Update test.js

--- javascript/test.js
+++ javascript/test.js
@@ -1,5 +1,6 @@
-require('./justc.node.js');
+const JUSTCWASM = require('./justc.node.js');
 (async ()=>{return await require('./justc.js')})().then((JUSTC)=>{
+    JUSTC.defineWASM(JUSTCWASM);
     console.log(typeof JUSTC, JUSTC, typeof JUSTC.initialize);
     JUSTC.initialize().then(()=>{
         console.log(JSON.stringify(JUSTC.execute('a is 123456.')))

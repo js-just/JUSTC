e99c32ccb2843ebb30905c3f6e562414a3ad2e40
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-11-09 19:02:45 +0500

    Update compile.sh

--- javascript/compile.sh
+++ javascript/compile.sh
@@ -38,8 +38,8 @@ rm -rf wabt-1.0.34 wabt-1.0.34-ubuntu.tar.gz
 
 SOURCE_FILES="core/js.cpp core/lexer.cpp core/parser.cpp core/from.json.cpp core/to.json.cpp core/keywords.cpp core/fetch.cpp core/to.xml.cpp core/to.yaml.cpp core/utility.cpp core/import.cpp"
 
-COMMON_FLAGS="-s EXPORTED_FUNCTIONS='[\"_lexer\",\"_parser\",\"_parse\",\"_free_string\",\"_malloc\",\"_free\",\"_version\"]' \
--s EXPORTED_RUNTIME_METHODS='[\"ccall\",\"cwrap\",\"UTF8ToString\",\"stringToUTF8\"]' \
+COMMON_FLAGS="-s EXPORTED_FUNCTIONS=[\"_lexer\",\"_parser\",\"_parse\",\"_free_string\",\"_malloc\",\"_free\",\"_version\"] \
+-s EXPORTED_RUNTIME_METHODS=[\"ccall\",\"cwrap\",\"UTF8ToString\",\"stringToUTF8\"] \
 -s MODULARIZE=1 \
 -s ALLOW_MEMORY_GROWTH=1 \
 -s INVOKE_RUN=0 \

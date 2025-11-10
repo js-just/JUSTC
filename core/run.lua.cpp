077789d6b1f4cba2950a714ca24b729d1c710b7d
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-11-10 19:23:09 +0500

    updates

--- core/run.lua.cpp
+++ core/run.lua.cpp
@@ -1,5 +1,32 @@
+/*
+
+MIT License
+
+Copyright (c) 2025 JustStudio. <https://juststudio.is-a.dev/>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 #include "run.lua.hpp"
 #include <iostream>
+#include "version.h"
 
 RunLua::RunLua() {
     L = luaL_newstate();
@@ -45,9 +72,11 @@ void RunLua::registerFunction(const std::string& name, lua_CFunction func) {
     }
 }
 
+int justc_version_lua(lua_State* L) {
+    lua_pushstring(L, JUSTC_VERSION);
+    return 1;
+}
+
 void RunLua::initJUSTC() {
-    registerFunction("justc_version", [](lua_State* L) -> int {
-        luacpp11::push(L, "1.0.0");
-        return 1;
-    });
+    registerFunction("justc_version", justc_version_lua);
 }
--- core/run.lua.hpp
+++ core/run.lua.hpp
@@ -1,12 +1,36 @@
+/*
+
+MIT License
+
+Copyright (c) 2025 JustStudio. <https://juststudio.is-a.dev/>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 #pragma once
 
 extern "C" {
-    #include <lua.hpp>
+    #include <lua.h>
     #include <lualib.h>
     #include <lauxlib.h>
 }
-
-#include "../third-party/progschj/luacpp11.hpp"
 
 class RunLua {
 public:
@@ -17,26 +41,6 @@ public:
     bool executeFile(const std::string& filename);
 
     void registerFunction(const std::string& name, lua_CFunction func);
-
-    template<typename Func>
-    void registerFunction(const std::string& name, Func&& func) {
-        lua_pushcfunction(L, luacpp11::push_callable(L, std::forward<Func>(func)));
-        lua_setglobal(L, name.c_str());
-    }
-
-    template<typename T>
-    T getGlobal(const std::string& name) {
-        lua_getglobal(L, name.c_str());
-        T result = luacpp11::to<T>(L, -1);
-        lua_pop(L, 1);
-        return result;
-    }
-
-    template<typename T>
-    void setGlobal(const std::string& name, T&& value) {
-        luacpp11::push(L, std::forward<T>(value));
-        lua_setglobal(L, name.c_str());
-    }
 
 private:
     lua_State* L;

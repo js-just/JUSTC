e65dbf96643dc9da39bfdb4dbb5d75ca48d4a190
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-11-11 15:00:27 +0500

    fix

--- javascript/compile.sh
+++ javascript/compile.sh
@@ -82,7 +82,7 @@ install_luau() {
 
     git clone https://github.com/luau-lang/luau.git
     cd luau
-    git checkout 0.994b6416f1a2d16ac06c52b4e574bad5d8749053
+    git checkout 994b6416f1a2d16ac06c52b4e574bad5d8749053
 
     echo "Building Luau for Emscripten..."
 

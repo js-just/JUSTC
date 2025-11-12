c0e087afc463f96a63a0bb9a7b1d3475fb3d170b
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-11-12 17:09:57 +0500

    Update main.cpp

--- core/main.cpp
+++ core/main.cpp
@@ -208,7 +208,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    OutputRedirector* outputRedirector = nullptr;
+    // OutputRedirector* outputRedirector = nullptr;
 
     try {
         cmdFlags flags;

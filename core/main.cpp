d6434a2cec30dbebc7f380231d721467c2ece5ef
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-11-13 13:39:13 +0500

    Update main.cpp

--- core/main.cpp
+++ core/main.cpp
@@ -380,6 +380,9 @@ int main(int argc, char* argv[]) {
         }
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
+    } catch (...) {
+        std::cerr << "Unknown fatal error." << std::endl;
+        return 1;
     }
 
     return 0;

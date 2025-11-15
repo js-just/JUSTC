a493f376ecefadfcdc5d10f83c1abe34326433fe
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-11-15 12:30:40 +0500

    Update compile.sh

--- javascript/compile.sh
+++ javascript/compile.sh
@@ -58,12 +58,12 @@ COMMON_FLAGS="-s EXPORTED_FUNCTIONS=[\"_lexer\",\"_parser\",\"_parse\",\"_free_s
 --bind \
 -I./third-party"
 
-WEB_FLAGS="-s ENVIRONMENT=web,worker\
--sASYNCIFY_IMPORTS=['fetch','emscripten_fetch','emscripten_fetch_close','use_luau']"
+WEB_FLAGS="-s ENVIRONMENT=web,worker \
+-s ASYNCIFY_IMPORTS=['fetch','emscripten_fetch','emscripten_fetch_close','use_luau']"
 WEB_OUTPUT="javascript/$SAFE_DIR/justc.core.js"
 
-NODE_FLAGS="-s ENVIRONMENT=node\
--sASYNCIFY_IMPORTS=['fetch','emscripten_fetch','emscripten_fetch_close']"
+NODE_FLAGS="-s ENVIRONMENT=node \
+-s ASYNCIFY_IMPORTS=['fetch','emscripten_fetch','emscripten_fetch_close']"
 NODE_OUTPUT="javascript_output/$SAFE_DIR/justc.node.js"
 
 JSOUT_DIR="javascript_output/$SAFE_DIR"

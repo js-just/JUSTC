5325ac36d0d2e9bc6ccb48b62efbc40af50e475a
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-11-24 23:16:26 +0500

    Update compress.js

--- javascript/compress.js
+++ javascript/compress.js
@@ -1,7 +1,16 @@
 const [file] = process.argv.slice(2);
+console.log('running compressor for', file);
 const fs = require('fs');
 const JSSC = require('strc');
 
-fs.readFile(file).then((content) => {
-    fs.writeFileSync(JSSC.compress(content));
-});
+console.log('reading', file);
+try {
+    const content = fs.readFileSync(file);
+    console.log('compressing', file);
+    const compressed = JSSC.compress(content);
+    console.log('writing', file);
+    fs.writeFileSync(file, compressed, 'utf16le');
+    console.log(file, 'compressed successfully');
+} catch (error) {
+    console.log('::error::', file, error);
+}

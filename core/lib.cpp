492696f7d2668bffa127cd3b4f000f0ba21925d7
JustDeveloper <176615419+justdevw@users.noreply.github.com>
2026-05-18 18:02:41 +0500

    Revert "Fix"
    This reverts commit 2b2ccde69d02d98dc23039c6b2c95a1e45e34888.

--- core/lib.cpp
+++ core/lib.cpp
@@ -40,18 +40,15 @@ Value API::convertFromCore(const ::Value& v) {
         case DataType::HEXADECIMAL:
         case DataType::BINARY:
         case DataType::OCTAL:
-            return std::visit([](auto&& num) -> Value {
-                return Value(num);
-            }, v.number_value);
+            return Value(v.number_value);
+
 
         case DataType::BIGNUM:
         case DataType::LARGENUM:
         case DataType::HUGENUM:
         case DataType::GIANTNUM:
         case DataType::COLOSSALNUM:
-            return std::visit([](auto&& num) -> Value {
-                return Value(static_cast<double>(num));
-            }, v.number_value);
+            return Value(v.number_value.convert_to<double>());
 
         case DataType::STRING:
         case DataType::LINK:

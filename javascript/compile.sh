a665a8458e50fea58f0ef865cb831c65b959eeaf
JustDeveloper <176615419+justdevw@users.noreply.github.com>
2026-05-18 18:01:42 +0500

    Revert "Update compile.sh"
    This reverts commit a0fc6baec2d06182feea96b6c5881e730658f9b6.

--- javascript/compile.sh
+++ javascript/compile.sh
@@ -79,6 +79,7 @@ COMMON_FLAGS="-s EXPORTED_FUNCTIONS=[\"_lexer\",\"_parser\",\"_parse\",\"_free_s
 -s ASYNCIFY=1 \
 -s FETCH=1 \
 -O0 \
+-gsource-map \
 -s ASSERTIONS=2 \
 -s SAFE_HEAP=1 \
 -s STACK_OVERFLOW_CHECK=2 \
@@ -203,7 +204,7 @@ for file in justc justc.node; do
         tail -n +2 "$JSOUT_DIR/$file.wat"
     } > $JSOUT_DIR/$file.tmp
     wat2wasm $JSOUT_DIR/$file.tmp --enable-annotations -o $JSOUT_DIR/$file.wasm
-    rm -f $JSOUT_DIR/$file.tmp $JSOUT_DIR/$file.wat
+    rm $JSOUT_DIR/$file.tmp
 done
 
 npm i strc

191a17836f1abf92233ab0352b06152f342ec762
JustDeveloper <176615419+justdevw@users.noreply.github.com>
2026-06-08 00:05:47 +0500

    Update run.lua.cpp

--- core/run.lua.cpp
+++ core/run.lua.cpp
@@ -36,11 +36,168 @@ SOFTWARE.
 #include <thread>
 #include <atomic>
 
+#pragma push_macro("LUA_H")
+#pragma push_macro("LUALIB_H")
+#pragma push_macro("LAUXLIB_H")
+#undef LUA_H
+#undef LUALIB_H
+#undef LAUXLIB_H
+
+#define lua_State lua_State_orig
+#define lua_newstate lua_newstate_orig
+#define lua_close lua_close_orig
+#define luaL_newstate luaL_newstate_orig
+#define luaL_openlibs luaL_openlibs_orig
+#define luaL_loadstring luaL_loadstring_orig
+#define lua_pcall lua_pcall_orig
+#define lua_tostring lua_tostring_orig
+#define lua_pop lua_pop_orig
+#define lua_type lua_type_orig
+#define lua_typename lua_typename_orig
+#define lua_tonumber lua_tonumber_orig
+#define lua_toboolean lua_toboolean_orig
+#define lua_touserdata lua_touserdata_orig
+#define lua_getglobal lua_getglobal_orig
+#define lua_setglobal lua_setglobal_orig
+#define lua_pushnil lua_pushnil_orig
+#define lua_pushnumber lua_pushnumber_orig
+#define lua_pushstring lua_pushstring_orig
+#define lua_next lua_next_orig
+#define lua_getfield lua_getfield_orig
+#define lua_getmetatable lua_getmetatable_orig
+#define lua_objlen lua_objlen_orig
+#define lua_gettop lua_gettop_orig
+#define lua_settop lua_settop_orig
+#define lua_pushvalue lua_pushvalue_orig
+#define lua_insert lua_insert_orig
+#define lua_remove lua_remove_orig
+#define lua_replace lua_replace_orig
+#define lua_createtable lua_createtable_orig
+#define lua_setfield lua_setfield_orig
+#define lua_rawgeti lua_rawgeti_orig
+#define lua_rawseti lua_rawseti_orig
+#define lua_gettable lua_gettable_orig
+#define lua_settable lua_settable_orig
+
 extern "C" {
     #include "lua.h"
     #include "lualib.h"
     #include "lauxlib.h"
 }
+
+#undef lua_State
+#undef lua_newstate
+#undef lua_close
+#undef luaL_newstate
+#undef luaL_openlibs
+#undef luaL_loadstring
+#undef lua_pcall
+#undef lua_tostring
+#undef lua_pop
+#undef lua_type
+#undef lua_typename
+#undef lua_tonumber
+#undef lua_toboolean
+#undef lua_touserdata
+#undef lua_getglobal
+#undef lua_setglobal
+#undef lua_pushnil
+#undef lua_pushnumber
+#undef lua_pushstring
+#undef lua_next
+#undef lua_getfield
+#undef lua_getmetatable
+#undef lua_objlen
+#undef lua_gettop
+#undef lua_settop
+#undef lua_pushvalue
+#undef lua_insert
+#undef lua_remove
+#undef lua_replace
+#undef lua_createtable
+#undef lua_setfield
+#undef lua_rawgeti
+#undef lua_rawseti
+#undef lua_gettable
+#undef lua_settable
+
+using lua_State = struct lua_State_orig;
+using lua_CFunction = int (*)(lua_State_orig*);
+
+extern "C" {
+    lua_State_orig* luaL_newstate_orig(void);
+    void luaL_openlibs_orig(lua_State_orig* L);
+    int luaL_loadstring_orig(lua_State_orig* L, const char* s);
+    int lua_pcall_orig(lua_State_orig* L, int nargs, int nresults, int errfunc);
+    const char* lua_tostring_orig(lua_State_orig* L, int idx);
+    void lua_pop_orig(lua_State_orig* L, int n);
+    int lua_type_orig(lua_State_orig* L, int idx);
+    const char* lua_typename_orig(lua_State_orig* L, int tp);
+    lua_Number lua_tonumber_orig(lua_State_orig* L, int idx);
+    int lua_toboolean_orig(lua_State_orig* L, int idx);
+    void* lua_touserdata_orig(lua_State_orig* L, int idx);
+    void lua_getglobal_orig(lua_State_orig* L, const char* name);
+    void lua_setglobal_orig(lua_State_orig* L, const char* name);
+    void lua_pushnil_orig(lua_State_orig* L);
+    void lua_pushnumber_orig(lua_State_orig* L, lua_Number n);
+    void lua_pushstring_orig(lua_State_orig* L, const char* s);
+    int lua_next_orig(lua_State_orig* L, int idx);
+    void lua_getfield_orig(lua_State_orig* L, int idx, const char* k);
+    int lua_getmetatable_orig(lua_State_orig* L, int idx);
+    size_t lua_objlen_orig(lua_State_orig* L, int idx);
+    int lua_gettop_orig(lua_State_orig* L);
+    void lua_settop_orig(lua_State_orig* L, int idx);
+    void lua_pushvalue_orig(lua_State_orig* L, int idx);
+    void lua_insert_orig(lua_State_orig* L, int idx);
+    void lua_remove_orig(lua_State_orig* L, int idx);
+    void lua_replace_orig(lua_State_orig* L, int idx);
+    void lua_createtable_orig(lua_State_orig* L, int narr, int nrec);
+    void lua_setfield_orig(lua_State_orig* L, int idx, const char* k);
+    void lua_rawgeti_orig(lua_State_orig* L, int idx, int n);
+    void lua_rawseti_orig(lua_State_orig* L, int idx, int n);
+    void lua_gettable_orig(lua_State_orig* L, int idx);
+    void lua_settable_orig(lua_State_orig* L, int idx);
+    void lua_close_orig(lua_State_orig* L);
+    int luaL_loadstringx_orig(lua_State_orig* L, const char* s, const char* mode);
+}
+
+inline lua_State* luaL_newstate() { return (lua_State*)luaL_newstate_orig(); }
+inline void luaL_openlibs(lua_State* L) { luaL_openlibs_orig((lua_State_orig*)L); }
+inline int luaL_loadstring(lua_State* L, const char* s) { return luaL_loadstring_orig((lua_State_orig*)L, s); }
+inline int lua_pcall(lua_State* L, int nargs, int nresults, int errfunc) { return lua_pcall_orig((lua_State_orig*)L, nargs, nresults, errfunc); }
+inline const char* lua_tostring(lua_State* L, int idx) { return lua_tostring_orig((lua_State_orig*)L, idx); }
+inline void lua_pop(lua_State* L, int n) { lua_pop_orig((lua_State_orig*)L, n); }
+inline int lua_type(lua_State* L, int idx) { return lua_type_orig((lua_State_orig*)L, idx); }
+inline const char* lua_typename(lua_State* L, int tp) { return lua_typename_orig((lua_State_orig*)L, tp); }
+inline double lua_tonumber(lua_State* L, int idx) { return lua_tonumber_orig((lua_State_orig*)L, idx); }
+inline int lua_toboolean(lua_State* L, int idx) { return lua_toboolean_orig((lua_State_orig*)L, idx); }
+inline void* lua_touserdata(lua_State* L, int idx) { return lua_touserdata_orig((lua_State_orig*)L, idx); }
+inline void lua_getglobal(lua_State* L, const char* name) { lua_getglobal_orig((lua_State_orig*)L, name); }
+inline void lua_setglobal(lua_State* L, const char* name) { lua_setglobal_orig((lua_State_orig*)L, name); }
+inline void lua_pushnil(lua_State* L) { lua_pushnil_orig((lua_State_orig*)L); }
+inline void lua_pushnumber(lua_State* L, double n) { lua_pushnumber_orig((lua_State_orig*)L, n); }
+inline void lua_pushstring(lua_State* L, const char* s) { lua_pushstring_orig((lua_State_orig*)L, s); }
+inline int lua_next(lua_State* L, int idx) { return lua_next_orig((lua_State_orig*)L, idx); }
+inline void lua_getfield(lua_State* L, int idx, const char* k) { lua_getfield_orig((lua_State_orig*)L, idx, k); }
+inline int lua_getmetatable(lua_State* L, int idx) { return lua_getmetatable_orig((lua_State_orig*)L, idx); }
+inline size_t lua_objlen(lua_State* L, int idx) { return lua_objlen_orig((lua_State_orig*)L, idx); }
+inline int lua_gettop(lua_State* L) { return lua_gettop_orig((lua_State_orig*)L); }
+inline void lua_settop(lua_State* L, int idx) { lua_settop_orig((lua_State_orig*)L, idx); }
+inline void lua_pushvalue(lua_State* L, int idx) { lua_pushvalue_orig((lua_State_orig*)L, idx); }
+inline void lua_insert(lua_State* L, int idx) { lua_insert_orig((lua_State_orig*)L, idx); }
+inline void lua_remove(lua_State* L, int idx) { lua_remove_orig((lua_State_orig*)L, idx); }
+inline void lua_replace(lua_State* L, int idx) { lua_replace_orig((lua_State_orig*)L, idx); }
+inline void lua_createtable(lua_State* L, int narr, int nrec) { lua_createtable_orig((lua_State_orig*)L, narr, nrec); }
+inline void lua_setfield(lua_State* L, int idx, const char* k) { lua_setfield_orig((lua_State_orig*)L, idx, k); }
+inline void lua_rawgeti(lua_State* L, int idx, int n) { lua_rawgeti_orig((lua_State_orig*)L, idx, n); }
+inline void lua_rawseti(lua_State* L, int idx, int n) { lua_rawseti_orig((lua_State_orig*)L, idx, n); }
+inline void lua_gettable(lua_State* L, int idx) { lua_gettable_orig((lua_State_orig*)L, idx); }
+inline void lua_settable(lua_State* L, int idx) { lua_settable_orig((lua_State_orig*)L, idx); }
+inline void lua_close(lua_State* L) { lua_close_orig((lua_State_orig*)L); }
+
+#pragma pop_macro("LUA_H")
+#pragma pop_macro("LUALIB_H")
+#pragma pop_macro("LAUXLIB_H")
 
 #ifdef __EMSCRIPTEN__
     #include <emscripten.h>
